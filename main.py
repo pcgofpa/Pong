44ef243603c5b0528deb375f8e3f525e576bec53
pcgofpa <pcgofpa@gmail.com>
2023-07-15 10:12:04 -0500

    Created ball
    - created pong ball
    - set initial movement direction
    - ball bounce when hitting wall
    - ball bounce when hitting paddle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 
 game_on = True
 
@@ -11,6 +13,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,6 +22,15 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.initial_movement()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
+    elif ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 screen.exitonclick()
